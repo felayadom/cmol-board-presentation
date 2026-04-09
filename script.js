cf00d1a9a61de8ab332e457c0b26a2e623d625b5
felayadom <felayadom@gmail.com>
2026-04-09 13:33:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,6 +21,14 @@ function showSlide(index) {
 
   currentSlideEl.textContent = index + 1;
   currentSlide = index;
+  if (prevBtn) {
+  prevBtn.style.display = index === 0 ? 'none' : 'block';
+}
+
+if (nextBtn) {
+  nextBtn.style.display = index === slides.length - 1 ? 'none' : 'block';
+}
+}
 }
 
 function nextSlide() {
